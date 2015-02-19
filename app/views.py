d8a067fb337078fc3c56c87cfd6b1e74e4894b87
Jacob Errington <nictytan@gmail.com>
2015-02-19 02:07:44 -0500

    implement mentor request form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,37 @@
 from app import app
 
+from app.forms import MentorRequestForm
+from app.database import Database
+from app.auth import requires_auth
+
+from flask import render_template
+
 @app.route('/')
 def index():
     return "hello world"
+
+@app.route('/help', methods=['GET', 'POST'])
+def mentor_request():
+    form = MentorRequestForm()
+    if form.validate_on_submit():
+        try:
+            user_id = Database.create_user_if_not_exists(
+                    form.user_name.data,
+                    form.user_email.data)
+        except ValueError as e:
+            return render_template('message.html', message=str(e))
+
+        try:
+            ticket_id = Database.create_ticket(
+                    user_id,
+                    form.ticket_table_number.data,
+                    form.ticket_contents.data,
+                    'pending')
+        except ValueError as e:
+            return "database is totally borked"
+
+        return render_template('success.html')
+
+    return render_template('help.html',
+            title='Mentor me!',
+            form=form)
