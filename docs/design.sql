e96b3a3266a3afeba41b8782bdeec3d9c52fbd8a
Jacob Errington <nictytan@gmail.com>
2015-02-19 02:02:56 -0500

    make schema BETTER

--- docs/design.sql
+++ docs/design.sql
@@ -5,21 +5,38 @@ DROP TABLE IF EXISTS Hacker;
 DROP TABLE IF EXISTS TicketStatus;
 
 CREATE TABLE Hacker (
-    userId int PRIMARY KEY,
-    userName varchar(50) NOT NULL,
-    userEmail varchar(256) UNIQUE NOT NULL
+    userId
+        SERIAL PRIMARY KEY,
+    userName
+        varchar(50) NOT NULL,
+    userEmail
+        varchar(256) UNIQUE NOT NULL,
+    userRegisterTime
+        timestamp without time zone NOT NULL
+        default (now() at time zone 'utc')
 );
 
 CREATE TABLE TicketStatus (
-    ticketStatusId SERIAL PRIMARY KEY,
-    ticketStatusName varchar(15) UNIQUE NOT NULL
+    ticketStatusId
+        SERIAL PRIMARY KEY,
+    ticketStatusName
+        varchar(15) UNIQUE NOT NULL
 );
 
 CREATE TABLE Ticket (
-    ticketId SERIAL PRIMARY KEY,
-    userId int REFERENCES Hacker,
-    ticketContents varchar(150) NOT NULL,
-    ticketStatusId int REFERENCES ticketStatus NOT NULL
+    ticketId
+        SERIAL PRIMARY KEY,
+    userId
+        int REFERENCES Hacker,
+    ticketTableNumber
+        int NOT NULL,
+    ticketContents
+        varchar(150) NOT NULL,
+    ticketStatusId
+        int REFERENCES ticketStatus NOT NULL,
+    ticketCreationTime
+        timestamp without time zone NOT NULL
+        default (now() at time zone 'utc')
 );
 
 INSERT INTO Hacker ( userId, userName, userEmail ) VALUES ( 0, 'null', 'no email' );
@@ -31,6 +48,8 @@ INSERT INTO TicketStatus ( ticketStatusName ) VALUES ( 'pending' );
 INSERT INTO TicketStatus ( ticketStatusName ) VALUES ( 'confirmed' );
 INSERT INTO TicketStatus ( ticketStatusName ) VALUES ( 'closed' );
 
-INSERT INTO Ticket ( ticketId, userId, ticketContents, ticketStatusId ) VALUES ( 0, 0, 'empty ticket', 0 );
+INSERT INTO Ticket
+    ( ticketId, userId, ticketTableNumber, ticketContents, ticketStatusId )
+    VALUES ( 0, 0, 0, 'empty ticket', 0 );
 
 COMMIT;
