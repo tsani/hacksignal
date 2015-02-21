57287478e762cc60878c2fe83f53c806d9691cff
Jacob Errington <nictytan@gmail.com>
2015-02-21 12:27:32 -0500

    remove email validation

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class MentorRequestForm(Form):
     user_name = StringField('Full name', validators=[
         DataRequired(), Length(max=50)])
     user_email = StringField('Email address', validators=[
-        DataRequired(), Length(max=255), Email()])
+        DataRequired(), Length(max=255)])
     ticket_contents = TextAreaField("What's up? (max. 150 chars)", validators=[
         DataRequired(), Length(max=150)])
     ticket_table_number = IntegerField('Table number', validators=[
