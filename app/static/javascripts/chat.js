0fe76fa070f2878fa4dc9c9aa4cede3453591a4f
Jacob Errington <nictytan@gmail.com>
2015-03-22 19:00:53 -0400

    logging messages

--- app/static/javascripts/chat.js
+++ app/static/javascripts/chat.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+
+    console.log('connected');
     socket.on('my response', function(msg) {
         $('#chatlog').append('<p>Received: ' + msg.data + '</p>');
     });
+
+    console.log('registered socket event handler');
     $('#submit-button').click(function(event) {
         socket.emit('my event', {data: $('#msg-content').val()});
         return false;
     });
+
+    console.log('registered click event handler');
 });
