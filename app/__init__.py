3a88ad88e5953e9e84f819238ea093283ced5aa7
Jacob Errington <nictytan@gmail.com>
2015-03-22 18:30:45 -0400

    fix up socket io integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+from flask.ext.socketio import SocketIO
+
 app = Flask(__name__)
 
 app.config.from_object('secret_config')
 
+app.socketio = SocketIO(app)
+
 from app import views
 from app import api
 from app import config
+from app import chat
--- app/chat.py
+++ app/chat.py
@@ -1,13 +1,15 @@
-from flask.ext.socketio import SocketIO, emit
+from app import app
 
-@socketio.on('my event', namespace='/test')
+from flask.ext.socketio import emit
+
+@app.socketio.on('my event', namespace='/test')
 def test_message(message):
     emit('my response', {'data': message['data']})
 
-@socketio.on('connect', namespace='/test')
+@app.socketio.on('connect', namespace='/test')
 def test_connect():
     emit('my response', {'data': 'Connected'})
 
-@socketio.on('disconnect', namespace='/test')
+@app.socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
