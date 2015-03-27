31b0b6107956b3bc8274a3578d20de0668b9a91e
Jacob Errington <nictytan@gmail.com>
2015-03-27 18:50:19 -0400

    correct gunicorn script to use socketio server
    Also, use older version of gunicorn because newer ones are bugged for sockets.

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec gunicorn -k gevent -w 2 -n mchacks-mentorship -b 127.0.0.1:6543 app:app
+exec gunicorn --worker-class socketio.sgunicorn.GeventSocketIOWorker \
+    -n mchacks-mentorship -b 127.0.0.1:6543 app:app
