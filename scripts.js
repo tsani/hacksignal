a5344d9d00d1e02c4d83dcdbb88b7eb633453069
Arielle VanIderstine <arielle1459@gmail.com>
2015-03-28 21:28:48 -0400

    Fix scrolling distance

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $("#logo").click(function() {
 	$("body").addClass("light");
 	$("#hidden").addClass("red");
-	$("html, body").animate({scrollTop:600}, 1000);
+	$("html, body").animate({scrollTop:($("#section2")[0].scrollHeight)}, 1000);
 });
