c07631010c9d85fdb09b08d4e4b73995a0a7a97e
Arielle VanIderstine <arielle1459@gmail.com>
2015-03-29 20:37:13 -0400

    Screenshots, colour-changing buttons, wording

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
 $("#logo").click(function() {
 	$("body").addClass("light");
 	$("#hidden").addClass("red");
-	$("html, body").animate({scrollTop:($("#section2")[0].scrollHeight)}, 1000);
+	$('html, body').animate({
+        scrollTop: $('#section2')[0].scrollHeight
+    }, 500);
+});
+
+$(".center-button").click(function() {
+	$(".center-button").addClass("active");
+	$(".left-button").removeClass("active");
+	$(".right-button").removeClass("active");
+});
+
+$(".left-button").click(function() {
+	$(".center-button").removeClass("active");
+	$(".left-button").addClass("active");
+	$(".right-button").removeClass("active");
+});
+
+$(".right-button").click(function() {
+	$(".center-button").removeClass("active");
+	$(".left-button").removeClass("active");
+	$(".right-button").addClass("active");
 });
